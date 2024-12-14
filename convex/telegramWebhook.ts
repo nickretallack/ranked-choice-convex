9364fccd76f06adc161b47431d9a1dd45e51fe13
Nick Retallack <nickretallack@gmail.com>
2024-12-14 13:28:23 -0800

    telegram sends token to webhook

--- convex/telegramWebhook.ts
+++ convex/telegramWebhook.ts
@@ -9,10 +9,16 @@ bot.command("ping", (ctx) => ctx.reply(`Pong! ${new Date().toISOString()}`));
 const handleUpdate = webhookCallback(bot, "std/http");
 
 export const webhook = httpAction(async (ctx, req) => {
-  req.headers.forEach((value, key) => {
-    console.log(key, value);
-  });
-  return await handleUpdate(req);
+  try {
+    const url = new URL(req.url);
+    if (url.searchParams.get("token") !== bot.token) {
+      return new Response("not allowed", { status: 405 });
+    }
+    return await handleUpdate(req);
+  } catch (err) {
+    console.error(err);
+    return new Response();
+  }
 });
 
 export default webhook;
