43abd4e3e0de72cc864ead4c30054e203e183d17
Nick Retallack <nickretallack@gmail.com>
2024-12-18 21:23:43 -0800

    Seamless sign-in frontend

--- src/telegram/Start.tsx
+++ src/telegram/Start.tsx
@@ -1,147 +1,84 @@
 import { useCallback, useEffect, useState } from "react";
-import {
-  Authenticated,
-  Unauthenticated,
-  useAction,
-  useConvex,
-} from "convex/react";
+import { useAction } from "convex/react";
 import { api } from "@convex/_generated/api";
 import Telegram from "@twa-dev/sdk";
-import { useNavigate } from "react-router";
-import { useUser, useSignIn } from "@clerk/clerk-react";
+import { useLocation, useNavigate } from "react-router";
+import { useClerk, useSignIn, useUser } from "@clerk/clerk-react";
 
 export default function Start() {
+  const navigate = useNavigate();
+  const { pathname, search, hash } = useLocation();
   const validateUser = useAction(api.telegram.actions.validateUser);
-  const [loading, setLoading] = useState<boolean>(false);
-  const { signIn, setActive } = useSignIn();
-  const { user } = useUser();
+  const { isLoaded, signIn, setActive } = useSignIn();
+  const { signOut } = useClerk();
+  const [error, setError] = useState<string | null>(null);
+  const { user, isSignedIn } = useUser();
 
-  const signInHandler = useCallback(() => {
-    if (!signIn || !setActive || user || loading) {
-      return;
-    }
-    const createSignIn = async () => {
-      setLoading(true);
-      try {
-        const token = await validateUser({
-          initData: Telegram.initData,
-        });
+  const nextStep = useCallback(async () => {
+    const pollId = Telegram.initDataUnsafe.start_param;
+    await navigate(`/telegram/poll/${pollId}`);
+  }, [navigate]);
 
-        // Create the `SignIn` with the token
-        const signInAttempt = await signIn.create({
-          strategy: "ticket",
-          ticket: token,
-        });
+  const seamlessSignIn = useCallback(async () => {
+    if (!isLoaded) return;
+    await signOut({ redirectUrl: `${pathname}${search}${hash}` });
 
-        // If the sign-in was successful, set the session to active
-        if (signInAttempt.status === "complete") {
-          setActive({
-            session: signInAttempt.createdSessionId,
-          });
-          console.log("HOORAY");
-        } else {
-          // If the sign-in attempt is not complete, check why.
-          // User may need to complete further steps.
-          console.error(JSON.stringify(signInAttempt, null, 2));
-        }
-      } catch (err) {
-        // See https://clerk.com/docs/custom-flows/error-handling
-        // for more info on error handling
-        console.error("Error:", JSON.stringify(err, null, 2));
-      } finally {
-        setLoading(false);
+    // Check who's currently signed in
+    if (isSignedIn) {
+      if (
+        user.publicMetadata.telegramUserId === Telegram.initDataUnsafe.user!.id
+      ) {
+        // The correct user is already signed in.
+        return await nextStep();
+      } else {
+        // Someone else is signed in.  Log out and redirect back to here to try again.
+        return await signOut({ redirectUrl: `${pathname}${search}${hash}` });
       }
-    };
+    }
 
-    createSignIn().catch(console.error);
-  }, [signIn, setActive, user, loading, validateUser]);
+    // Get a clerk token
+    let token: string;
+    try {
+      token = await validateUser({
+        initData: Telegram.initData,
+      });
+    } catch (error) {
+      console.error(error);
+      return setError(String(error));
+    }
 
-  return <button onClick={signInHandler}>Sign in</button>;
-  // if (!user) {
-  //   return null;
-  // }
+    // Sign in with clerk
+    const signInAttempt = await signIn.create({
+      strategy: "ticket",
+      ticket: token,
+    });
 
-  // if (loading) {
-  //   return <div>Signing you in...</div>;
-  // }
+    if (signInAttempt.status === "complete") {
+      setActive({
+        session: signInAttempt.createdSessionId,
+      });
+      return await nextStep();
+    } else {
+      console.error(signInAttempt);
+      return setError(String(signInAttempt.status).replace("_", " "));
+    }
+  }, [
+    isLoaded,
+    isSignedIn,
+    user,
+    signIn,
+    signOut,
+    setActive,
+    validateUser,
+    nextStep,
+    pathname,
+    search,
+    hash,
+  ]);
 
-  // return <div>Signed in as {user.id}</div>;
+  useEffect(() => {
+    seamlessSignIn().catch(console.error);
+  }, [seamlessSignIn]);
+
+  return <div>{error ? error : "Just a moment..."}</div>;
 }
-
-// export function A() {
-//   const validateUser = useAction(api.telegram.actions.validateUser);
-//   const navigate = useNavigate();
-//   const { signIn, setActive } = useSignIn();
-
-//   const convex = useConvex();
-
-//   const [isInvalid, setIsInvalid] = useState(false);
-//   // useEffect(() => {
-//   //   // const handler = async () => {
-//   // convex.setAuth(
-//   //   async () => await validateUser({ initData: Telegram.initData }),
-//   //   (isAuthenticated) => {
-//   //     console.log("isAuthenticated", isAuthenticated);
-//   //     if (isAuthenticated) {
-//   //       const pollId = Telegram.initDataUnsafe.start_param;
-//   //       navigate(`/telegram/poll/${pollId}`) as void;
-//   //     } else {
-//   //       setIsInvalid(true);
-//   //     }
-//   //   },
-//   //   );
-//   // }, [convex, navigate, validateUser]);
-
-//   return (
-//     <div>
-//       <button
-//         onClick={() => {
-//           // const token = await validateUser({ initData: Telegram.initData });
-//           // const token =
-//           //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJqczdiMDhreGpuMDlrOXpxczY1cTBxc3c2OTc2aDFnMSIsImF1ZCI6ImNvbnZleCIsIm5hbWUiOiJTcXVpcmJvICIsInBpY3R1cmUiOiJodHRwczovL3QubWUvaS91c2VycGljLzMyMC9PVEhVZkVucjlsZmJ1S2NFRTBmejNJb0dJSHJLdVJNQ0NjOUl4RkNkRnN3LnN2ZyIsIm5pY2tuYW1lIjoiU3F1aXJibyIsImdpdmVuX25hbWUiOiJTcXVpcmJvIiwiZmFtaWx5X25hbWUiOiIifQ.YG1H81TddeFL-XVrnGmVM9QSQc72m4nwtDULQVd6G34";
-//           // console.log("token", token);
-
-//           validateUser({
-//             initData: Telegram.initData,
-//           }).then(
-//             async (token) => {
-//               const signInAttempt = await signIn!.create({
-//                 strategy: "ticket",
-//                 ticket: token,
-//               });
-
-//               console.log("token", token);
-
-//               convex.setAuth(
-//                 async () => {
-//                   return token;
-//                   // const token = await validateUser({
-//                   //   initData: Telegram.initData,
-//                   // });
-//                   // console.log("token", token);
-//                   // return token;
-//                 },
-//                 (isAuthenticated) => {
-//                   console.log("isAuthenticated", isAuthenticated);
-//                   if (isAuthenticated) {
-//                     const pollId = Telegram.initDataUnsafe.start_param;
-//                     navigate(`/telegram/poll/${pollId}`) as void;
-//                   } else {
-//                     setIsInvalid(true);
-//                   }
-//                 },
-//               );
-//             },
-//             (error) => console.error(error),
-//           );
-//         }}
-//       >
-//         Auth
-//       </button>
-//       {isInvalid ? "Invalid telegram user details." : "Just a moment..."}
-
-//       <Authenticated>Yes </Authenticated>
-//       <Unauthenticated>No</Unauthenticated>
-//     </div>
-//   );
-// }
