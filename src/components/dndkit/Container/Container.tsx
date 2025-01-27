adc05099fe3d7e6e85358b043b774b1567e2f268
Nick Retallack <nickretallack@gmail.com>
2025-01-27 08:57:59 -0800

    removed some unused properties from dndkit code

--- src/components/dndkit/Container/Container.tsx
+++ src/components/dndkit/Container/Container.tsx
@@ -1,36 +1,17 @@
-import React, { forwardRef } from "react";
 import classNames from "classnames";
+import React, { forwardRef } from "react";
 
 import styles from "./Container.module.css";
 
 export interface Props {
   children: React.ReactNode;
-  label?: string;
   style?: React.CSSProperties;
-  horizontal?: boolean;
-  hover?: boolean;
-  handleProps?: React.HTMLAttributes<any>;
-  shadow?: boolean;
   unstyled?: boolean;
-  onClick?(): void;
+  onClick?: () => void;
 }
 
 export const Container = forwardRef<HTMLDivElement, Props>(
-  (
-    {
-      children,
-      handleProps,
-      horizontal,
-      hover,
-      onClick,
-      label,
-      style,
-      shadow,
-      unstyled,
-      ...props
-    }: Props,
-    ref
-  ) => {
+  ({ children, onClick, style, unstyled, ...props }: Props, ref) => {
     return (
       <div
         {...props}
@@ -40,18 +21,12 @@ export const Container = forwardRef<HTMLDivElement, Props>(
             ...style,
           } as React.CSSProperties
         }
-        className={classNames(
-          styles.Container,
-          unstyled && styles.unstyled,
-          horizontal && styles.horizontal,
-          hover && styles.hover,
-          shadow && styles.shadow
-        )}
+        className={classNames(styles.Container, unstyled && styles.unstyled)}
         onClick={onClick}
         tabIndex={onClick ? 0 : undefined}
       >
         {children}
       </div>
     );
-  }
+  },
 );
