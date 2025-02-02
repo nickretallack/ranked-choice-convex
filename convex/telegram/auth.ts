b8bb107b85b9496dff53f5a8b06b735697d0c5a4
Nick Retallack <nickretallack@gmail.com>
2025-02-01 17:28:32 -0800

    attempting to do telegram auth provider

--- convex/telegram/auth.ts
+++ convex/telegram/auth.ts
@@ -1,22 +1,32 @@
-"use node";
-
-import { AuthProviderConfig } from "@convex-dev/auth/server";
+import { ConvexCredentials } from "@convex-dev/auth/providers/ConvexCredentials";
 import { validateWebAppData } from "@grammyjs/validator";
 import type { WebAppUser } from "@twa-dev/types";
 import { api } from "../_generated/api";
 
-export const TelegramProvider: AuthProviderConfig = {
-  id: "telegram",
-  type: "credentials",
-  authorize: async (params, ctx) => {
-    const telegramUserDetails = validateInitData(params.initData as string);
-
+export const TelegramProvider = ConvexCredentials({
+  authorize: async (credentials, ctx) => {
+    const telegramUserDetails = validateInitData(
+      credentials.initData as string,
+    );
     const { user } = await ctx.runMutation(api.telegram.user.upsert, {
       telegramUserDetails,
     });
     return { userId: user._id };
   },
-};
+});
+
+// export const TelegramProvider: AuthProviderConfig = {
+//   id: "telegram",
+//   type: "credentials",
+//   authorize: async (params, ctx) => {
+//     const telegramUserDetails = validateInitData(params.initData as string);
+
+//     const { user } = await ctx.runMutation(api.telegram.user.upsert, {
+//       telegramUserDetails,
+//     });
+//     return { userId: user._id };
+//   },
+// };
 
 export function validateInitData(initData: string) {
   const params = new URLSearchParams(initData);
